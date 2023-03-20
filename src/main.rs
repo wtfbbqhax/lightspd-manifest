03ec60a16909127a1b55543503882ae3bba2a916
Victor Roemer <victor@badsec.org>
2023-03-19 18:10:58 -0700

    lightspd-manifest: Another implementation using vector map but is less useful

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+use std::collections::HashSet;
 use std::collections::HashMap;
-use std::env;
 use std::fs::File;
 use std::io::{ BufReader, Read };
 use flate2::read::GzDecoder;
-use indexmap::IndexMap;
 use tar::Archive;
 
 mod version;
@@ -14,7 +13,16 @@ static EXCLUDE_PATHS: &[&str] = &[
     "lightspd/modules/src/"
 ];
 
-type AssetsMap = IndexMap<(String, version::Version), Vec<String>>;
+type Asset = (String, version::Version, String);
+
+fn remove_matching_elements(vec_a: &mut Vec<Asset>, vec_b: &Vec<(String, version::Version)>) {
+    // Create a set of tuples containing the first string and version from vec_b
+    let set_b: HashSet<(&String, &version::Version)> = vec_b.iter().map(|(s, v)| (s, v)).collect();
+
+    // Filter out all elements from vec_a whose first string and version match those in set_b
+    vec_a.retain(|(s, v, _)| !set_b.contains(&(s, v)));
+}
+
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
@@ -25,32 +33,28 @@ fn main() -> std::io::Result<()> {
     }
 
     let snort_version = version::parse_version_string(&args[1]).unwrap();
-    let snort_arch = &args[2];
-    let file = File::open(&args[3])?;
-    let reader = BufReader::new(file);
-    let gz = GzDecoder::new(reader);
-    let mut archive = Archive::new(gz);
-
-    let disobey = env::var("DISOBEY_PATRICK").unwrap_or(String::new());
-    let disobey = match disobey.to_lowercase().as_str() {
-        "true" | "yes" | "on" | "1" => true,
-        _ => false,
-    };
-
     if snort_version.any() {
         eprintln!("Error: invalid snort version: {}", &args[1]);
         std::process::exit(2);
     }
 
+
+    let snort_arch = &args[2];
+    let file = File::open(&args[3])?;
+    let reader = BufReader::new(file);
+    let gz = GzDecoder::new(reader);
+    let mut archive = Archive::new(gz);
     let mut lightspd_version = String::new();
 
     // There are several subdirs that must always be retained
-    let keep_rules: &[&str] = &[ "3.0.0.0" ];
+    //let keep_rules: &[&str] = &[ "3.0.0.0" ];
     let keep_modules: &[&str] = &[ "stubs" ];
     let keep_modules_arch: &[&str] = &[ snort_arch ];
 
+
+    let mut assets: Vec<Asset> = Vec::new();
+
     // Collect all the assets in the lightspd package into a versioned map
-    let mut map: AssetsMap = IndexMap::new();
     for file in archive.entries().unwrap() {
         let file = file.unwrap();
         let header = file.header();
@@ -66,49 +70,29 @@ fn main() -> std::io::Result<()> {
 
         if path.components().count() >= 4 {
             let mut components = path.components();
-            let _ = components.next().unwrap().as_os_str().to_str().unwrap();
+            let _ = components.next();
             let name = components.next().unwrap().as_os_str().to_str().unwrap();
             let ver = components.next().unwrap().as_os_str().to_str().unwrap();
             let arch = components.next().unwrap().as_os_str().to_str().unwrap();
 
             let version = version::parse_version_string(&ver).unwrap();
-            let version: version::Version = match name {
-                "rules" => {
-                    if keep_rules.contains(&ver) {
-                        version::Version::new(0, 0, 0, None, None)
-                    } else {
-                        version
-                    }
-                },
-                "modules" => {
-                    if !keep_modules.contains(&ver) && !keep_modules_arch.contains(&arch) {
-                        version::Version::new(999, 999, 999, None, None)
-                    } else {
-                        version
-                    }
-                },
-                _ => version,
-            };
 
-            if (disobey || name == "modules") && version > snort_version {
+            if version > snort_version {
                 continue;
             }
 
-            let key = (name.to_string(), version);
+            let name = name.to_string();
             let value = path.display().to_string();
-
-            map.entry(key)
-                .or_insert_with(Vec::new)
-                .push(value);
+            assets.push((name.to_string(), version, value));
         }
 
         // Special case, keep the lightspd/version.txt file.
         if path == std::path::PathBuf::from("lightspd/version.txt") {
-            let key = ("lightspd/version.txt".to_string(), version::Version::new(0, 0, 0, None, None));
+            let name = "lightspd/version.txt".to_string();
             let value = path.display().to_string();
-            map.entry(key)
-                .or_insert_with(Vec::new)
-                .push(value);
+            assets.push((name.to_string(), version::Version::new(0, 0, 0, None, None), value));
+
+            // Store the value of version.txt for reporting later.
             let mut file = file;
             file.read_to_string(&mut lightspd_version)?;
         }
@@ -117,23 +101,15 @@ fn main() -> std::io::Result<()> {
     // Identify all the versioned assets which are incompatible with our Snort version.
     let mut asset_versions: HashMap<String, version::Version> = HashMap::new();
     let mut keys_to_remove = Vec::new();
-    for (key, _) in &map {
-        let (name, version) = key;
+    for (name, version, _) in &assets {
+
         if version.any() {
             continue;
         }
+
         if let Some(previous_version) = asset_versions.get(name).cloned() {
-            // Disobey Talos and trash the majority of older cruft.
-            if disobey {
-                if *version > previous_version && *version <= snort_version {
-                    asset_versions.insert(name.clone(), version.clone());
-                    let key = (name.clone(), previous_version);
-                    keys_to_remove.push(key.clone());
-                } else {
-                    let key = (name.clone(), version.clone());
-                    keys_to_remove.push(key.clone());
-                }
-            } else if name == "modules" {
+            //let version = *version;
+            if name == "modules" {
                 if  *version > previous_version && *version <= snort_version {
                     asset_versions.insert(name.clone(), version.clone());
                     let key = (name.clone(), previous_version);
@@ -149,22 +125,18 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    // Remove the incompatible assets
-    for key in keys_to_remove {
-        map.remove(&key);
-    }
+
+    remove_matching_elements(&mut assets, &keys_to_remove);
 
     // Generate the manifest from remaining asset map
-    for (_, value) in &map {
-        for path in value {
-            println!("{}", path);
-        }
+    for (_, _, value) in &assets {
+        println!("{}", value);
     }
 
     // Print summary to stderr
     eprintln!("LightSPD {}", lightspd_version);
     eprintln!("Snort {} ", snort_version);
-    for ((name, version), _) in &map {
+    for (name, version, _) in &assets {
         if version.any() { continue; }
         eprintln!(" {} {}", name, version);
     }
